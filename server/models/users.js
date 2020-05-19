5e1bf6dc3b503df99e4c6caa174c5d7bd5898fa2
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-05-19 18:30:28 +0300

    adding the login and jwt functionality

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt"); // in order to hash the password
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+require('dotenv').config();
 // creating the schema
 
 const userSchema = mongoose.Schema({
@@ -60,6 +62,27 @@ userSchema.pre("save", function (next) {
     next();
   }
 });
+
+
+// creating method to compare password
+userSchema.methods.comparePassword = function(candidatePassword,cb){
+    bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
+        if(err) return cb(err);
+        cb(null,isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user =this;
+    var token = jwt.sign(user._id.toHexString(),process.env.SECRET)
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+
+    //the token is encryption of userId + password
+}
 
 // creating the model from the schema
 const User = mongoose.model("User", userSchema);
