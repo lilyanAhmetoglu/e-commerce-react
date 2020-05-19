1cb06517fe6e887c9445f4bab207bcf043c454ae
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-05-19 19:56:08 +0300

    implementing auth route and logout

--- server/server.js
+++ server/server.js
@@ -15,9 +15,35 @@ app.use(cookieParser());
 //Models
 const { User } = require("./models/users");
 
+//Middlewares
+const { auth } = require("./middleware/auth");
+
 //===========================
 //           USERS
 //============================
+app.get("/api/users/auth", auth, (req, res) => {
+  // we attached the user and token in request then we will jumb to this function
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
+
 app.post("/api/users/register", (req, res) => {
   // afer sending request we need to get the response
   const user = new User(req.body);
@@ -45,8 +71,8 @@ app.post("/api/users/login", (req, res) => {
       user.generateToken((err, user) => {
         if (err) return res.status(400).send(err);
         res.cookie("w_auth", user.token).status(200).json({
-            loginSuccess:true
-        })
+          loginSuccess: true,
+        });
       });
     });
   });
