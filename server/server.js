cb723c33a52944211a58d1cb0c13cc4f158d4a74
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-05-20 15:56:27 +0300

    adding brands

--- server/server.js
+++ server/server.js
@@ -14,10 +14,33 @@ app.use(cookieParser());
 
 //Models
 const { User } = require("./models/users");
+const { Brand } = require("./models/brands");
 
 //Middlewares
 const { auth } = require("./middleware/auth");
+const { admin } = require("./middleware/admin"); // we are going to create an admin middleware in order to check the user role
 
+//===========================
+//           BRANDS
+//============================
+
+app.post("/api/product/brand", auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
+  brand.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      brand: doc,
+    });
+  });
+});
+
+app.get("/api/product/brands", (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(brands);
+  });
+});
 //===========================
 //           USERS
 //============================
