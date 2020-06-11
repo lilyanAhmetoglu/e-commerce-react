b52c98dac39a49989b4e255ebad9ef6523c74798
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-06-11 15:57:19 +0300

    fixing packages problems

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Resources/css/styles.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Resources/css/styles.css";
 
-import Routes from './routes'
-import {BrowserRouter as Router } from  "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import Reducer from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 ReactDOM.render(
-  <Router>
-    <Routes />
-  </Router>,
-  document.getElementById('root')
-);
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  document.getElementById("root")
+);
