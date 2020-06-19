5e1727198536aab7bfa87b52e45c595a7541332c
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-06-19 14:20:00 +0300

    adding slider and promotion photo

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import HomeSlider from './home_slider';
+import HomePromotion from './home_promotion'
 export default class Home extends Component {
     render() {
         return (
             <div>
-                dddddd
+                <HomeSlider/>
+                <HomePromotion/>
             </div>
         )
     }
